57ba1ce7230bbed3394e0de206493f2ad30a52c0
Anthony <a.dowden@outlook.com>
2019-10-28 12:33:33 +0000

    renamed static-page = home

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable
+  
+  has_many :messages 
+  has_many :rideshares
+  has_many :passengers
+end
