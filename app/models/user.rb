7ca0f2f3624e5e16967efd3399b12fd68ce76574
Gabriel Oarcas <40719741+gabrieloarcas@users.noreply.github.com>
2019-11-21 19:28:24 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable
   
   has_many :messages 
   has_many :rideshares
