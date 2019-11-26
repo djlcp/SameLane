4a676408b09bc0f2997b4c5e49e48db0390fcd96
Matt Chatterley <matt@code.je>
2019-11-26 13:29:41 +0000

    Fixes for running in prod

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,4 @@
 class Passenger < ApplicationRecord
-  belongs_to :user, class_name => 'User', :foreign_key => 'user_id'
-  belongs_to :rideshare, class_name => 'Rideshare', :foreign_key => 'rideshare_id'
+  belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
+  belongs_to :rideshare, :class_name => 'Rideshare', :foreign_key => 'rideshare_id'
 end
