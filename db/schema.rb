a1e9a3f4e7ce961f0f3a1778a7f0e2481542edf1
gabrieloarcas <gabriel.oarcas@hotmail.com>
2019-12-05 15:52:24 +0000

    Add gravatar or upload own profile pic plus other styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_101945) do
+ActiveRecord::Schema.define(version: 2019_12_05_135624) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
@@ -18,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_101945) do
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["rideshare_id"], name: "fk_rails_a499cb2721"
+    t.index ["user_id"], name: "fk_rails_273a25a7a6"
   end
 
   create_table "passengers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -25,6 +48,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_101945) do
     t.bigint "rideshare_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["rideshare_id"], name: "fk_rails_e710398495"
+    t.index ["user_id"], name: "fk_rails_e53b659d9d"
   end
 
   create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -48,8 +73,11 @@ ActiveRecord::Schema.define(version: 2019_12_02_101945) do
     t.integer "seat"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "distance"
+    t.decimal "distance", precision: 10
     t.integer "travel_time"
+    t.index ["from_id"], name: "fk_rails_e4eeea412b"
+    t.index ["to_id"], name: "fk_rails_1f0681e884"
+    t.index ["user_id"], name: "fk_rails_58d169bbec"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -76,6 +104,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_101945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "rideshares"
   add_foreign_key "messages", "users"
   add_foreign_key "passengers", "rideshares"
