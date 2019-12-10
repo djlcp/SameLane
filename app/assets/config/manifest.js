3d03575a7d58da4ac7376ea338e1b09e4a51b4c3
Maddie <maria.madalyna@outlook.com>
2019-12-10 11:07:18 +0000

    Jquery trial

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
+//= require rails-ujs
+//= require activestorage
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require_tree .
 //= link_tree ../images
 //= link_directory ../stylesheets .css
